98507f2811f00c7a7078588910a39bad7b87dde4
IrtisumMusharratRahman <rhythmfakir@gmail.com>
2023-02-15 16:20:25 +0600

    basic websocket implementation with coinbase URL

--- app/src/main/java/com/example/hellofriends/MainActivity.kt
+++ app/src/main/java/com/example/hellofriends/MainActivity.kt
@@ -8,26 +8,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.Navigation
 import com.example.hellofriends.route.Navigation
 import com.example.hellofriends.ui.theme.HelloFriendsTheme
 import org.java_websocket.client.WebSocketClient
 import org.java_websocket.handshake.ServerHandshake
 import java.lang.Exception
-import java.net.SocketAddress
 import java.net.URI
-import java.util.Objects
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var webSocketClient:WebSocketClient
     val uri: URI = URI(WEB_SOCKET_URL)
+
     companion object {
         const val WEB_SOCKET_URL = "wss://ws-feed.pro.coinbase.com"
+//        const val WEB_SOCKET_URL = "ws://localhost:8088/websocket"
     }
+
+    var  msg:MutableState<String> = mutableStateOf("")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +39,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Navigation(){}
+//                    Navigation(){}
+                    Testing(msg.value)
                 }
             }
         }
@@ -46,28 +48,31 @@ class MainActivity : ComponentActivity() {
 
     override fun onStart() {
         super.onStart()
-        Log.e("TAG", "onStart: ", )
+        Log.e("TAG", "onStart: ")
         webSocketClient = object : WebSocketClient(uri) {
             override fun onOpen(handshakedata: ServerHandshake?) {
                 Log.e("TAG", "onOpen: ")
                 subscribe()
+//                webSocketClient.send("websocket")
             }
 
             override fun onMessage(message: String?) {
-                TODO("Not yet implemented")
                 Log.e("TAG", "onMessage: ${message}")
+                val str = message.orEmpty()
+                msg.value=str.take(str.length)
             }
 
             override fun onClose(code: Int, reason: String?, remote: Boolean) {
-                TODO("Not yet implemented")
+                Log.e("TAG", "onClose: ")
+                unsubscribe()
             }
 
             override fun onError(ex: Exception?) {
-                TODO("Not yet implemented")
                 Log.e("TAG", "onError: ${ex?.message}")
             }
 
         }
+        webSocketClient.connect()
     }
 
     override fun onStop() {
@@ -83,17 +88,24 @@ class MainActivity : ComponentActivity() {
                     "}"
         )
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HelloFriendsTheme {
-        Greeting("Android")
+    private fun unsubscribe(){
+        webSocketClient.send(
+            "{\n" +
+                    "    \"type\": \"unsubscribe\",\n" +
+                    "    \"channels\": [\"ticker\"]\n" +
+                    "}"
+        )
     }
+}
+
+@Composable
+fun Testing(msg:String) {
+    var message by remember {
+        mutableStateOf("", policy = structuralEqualityPolicy())
+    }
+    if (msg!=null){
+        message=msg
+    }
+    Text(text = message)
 }
--- app/src/main/java/com/example/hellofriends/route/Navigation.kt
+++ app/src/main/java/com/example/hellofriends/route/Navigation.kt
@@ -5,7 +5,6 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.hellofriends.Greeting
 
 @Composable
 fun Navigation(getNavController: (navController: NavController)-> Unit) {
@@ -15,7 +14,7 @@ fun Navigation(getNavController: (navController: NavController)-> Unit) {
     NavHost(navController = navController, startDestination = NavScreens.ChatScreen.route ){
 
         composable(route = NavScreens.ChatScreen.route){
-            Greeting("Android")
+//            Greeting("Android")
         }
 
     }
